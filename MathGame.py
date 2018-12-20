7c054a2ee7474d925a884afdd1127a4ddaaf03b7
RedPat <34812738+RedPat@users.noreply.github.com>
2018-12-20 14:34:21 +0100

    Create MathGame.py

---
+++ MathGame.py
@@ -0,0 +1,21 @@
+# values
+a = 5
+b = 3
+
+# calculation
+c = a + b
+
+# task
+print("Die Aufgabe:", a, "+", b)
+
+#input
+print("Bitte Eingabe tätigen.")
+x = input()
+print("Ihre Eingabe:", x)
+
+# conversion
+intX = int(x)
+print("Ihre Eingabe als Integer:", intX)
+
+# output
+print("Das Ergebnis:", c)
