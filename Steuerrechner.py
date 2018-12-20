b6cd7f9ac54d756507e5fceb35106f2098802bbe
RedPat <34812738+RedPat@users.noreply.github.com>
2018-12-20 14:34:48 +0100

    Create Steuerrechner.py

---
+++ Steuerrechner.py
@@ -0,0 +1,9 @@
+# values
+steuer = 0.18
+
+# input
+print("Bitte geben Sie Ihr Bruttogehalt ein.")
+gehalt = int(input())
+
+# output
+print("Es ergibt sich ein Steuerbetrag von:", gehalt*steuer, "Euro")
